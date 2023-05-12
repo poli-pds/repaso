34b58b0db3720c77943b2daa878bd4fea2703309
Giovanny Vargas <yovaz05@gmail.com>
2023-05-11 19:14:54 -0500

    clase 11-05-2023

---
+++ src/main/java/co/com/poli/repasopds/RepasopdsApplication.java
@@ -0,0 +1,13 @@
+package co.com.poli.repasopds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasopdsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepasopdsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/com/poli/repasopds/RepasopdsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.poli.repasopds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasopdsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
